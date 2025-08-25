fe7b9a1cd2bd507ac9b190a408a05e6ec89c3dee
Arshia Ghafoori <arshia001@live.com>
2025-08-25 16:22:12 +0400

    Update WASIX build scripts

--- wasix-build.sh
+++ wasix-build.sh
@@ -2,18 +2,49 @@
 
 set -exuo pipefail
 
-for dir in . psycopg psycopg_c psycopg_pool; do
+for dir in . psycopg psycopg_binary psycopg_c psycopg_pool; do
   rm -rf $dir/build $dir/dist $dir/*.egg-info
 done
 
 source .cross-venv/bin/activate
 
-export WASIXCC_SYSROOT=/home/arshia/repos/wasmer/wasix-libc/sysroot32-ehpic/
+if [ ! -d psycopg_binary ]; then
+  python3 ./tools/build/copy_to_binary.py
+fi
+
+DEPS_DIR=$(realpath ../python-wasix-binaries)
+
 export WASIXCC_COMPILER_FLAGS=-Wno-unreachable-code-fallthrough:-D__linux__
+export WASIXCC_LINKER_FLAGS="-L$DEPS_DIR/pgsql/lib:-lpq:-rpath=\$ORIGIN"
 export WASIXCC_WASM_EXCEPTIONS=yes
 export WASIXCC_PIC=yes
-export PG_INCLUDE_DIR=$(pwd)/../python-wasix-binaries/pgsql/include
-export PG_LIB_DIR=$(pwd)/../python-wasix-binaries/pgsql/lib
-python3 -m build psycopg_c --wheel --outdir dist
-python3 -m build psycopg_pool --wheel --outdir dist
-python3 -m build psycopg --wheel --outdir dist
+export PG_INCLUDE_DIR=$DEPS_DIR/pgsql/include
+export PG_LIB_DIR=$DEPS_DIR/pgsql/lib
+python3 -m build psycopg_c --wheel --outdir dist --no-isolation
+python3 -m build psycopg_binary --wheel --outdir dist --no-isolation
+python3 -m build psycopg_pool --wheel --outdir dist --no-isolation
+python3 -m build psycopg --wheel --outdir dist --no-isolation
+
+# Embed libpq into the binary packages, so it can be loaded automatically
+# via the $ORIGIN RUNPATH specified above.
+cd dist
+
+rm -rf temp
+mkdir temp
+unzip psycopg_binary-3.2.9-cp313-cp313-wasix_wasm32.whl -d temp
+cp -L $DEPS_DIR/pgsql/lib/libpq.so temp/psycopg_binary/
+rm psycopg_binary-3.2.9-cp313-cp313-wasix_wasm32.whl
+cd temp
+zip -r ../psycopg_binary-3.2.9-cp313-cp313-wasix_wasm32.whl *
+cd ..
+
+rm -rf temp
+mkdir temp
+unzip psycopg_c-3.2.9-cp313-cp313-wasix_wasm32.whl -d temp
+cp -L $DEPS_DIR/pgsql/lib/libpq.so temp/psycopg_c/
+rm psycopg_c-3.2.9-cp313-cp313-wasix_wasm32.whl
+cd temp
+zip -r ../psycopg_c-3.2.9-cp313-cp313-wasix_wasm32.whl *
+cd ..
+
+rm -rf temp
